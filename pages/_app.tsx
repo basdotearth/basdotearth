01fdf0aee6355eb583419c01783ee89bf1222c92
Bas Klinkhamer <bas@position-fixed.nl>
2022-09-21 10:13:34 +0200

    Post init cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/reset.css';
+import '../styles/globals.css';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default App;
