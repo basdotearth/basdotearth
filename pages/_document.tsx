1ecb4d95bcf8945cec994c8d62d270ea7ff7d5ea
Bas Klinkhamer <bas@position-fixed.nl>
2022-09-22 12:40:58 +0200

    Generate google fonts url

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,29 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+type TFontDefinition = {
+  name: string;
+  weights: number[],
+};
+
+const generateGoogleUrl = (fonts: TFontDefinition[]): string => {
+  const requested = fonts
+    .map(font => `family=${font.name.replace(/\s/g, '+')}:wght@${font.weights.join(';')}`)
+    .join('&');
+  return `https://fonts.googleapis.com/css2?${requested}&display=swap`;
+};
+
 const Document = () => {
+  const fonts: TFontDefinition[] = [{
+    name: 'Palanquin',
+    weights: [300, 400, 700],
+  }, {
+    name: 'Fira Code',
+    weights: [400, 600],
+  }];
+
   return <Html>
     <Head>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Palanquin:wght@300;500&family=Fira+Code:wght@400;600&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Palanquin:wght@700&text=BasKlinkhmer&display=swap"
-        rel="stylesheet"
-      />
+      <link href={generateGoogleUrl(fonts)} rel="stylesheet" />
     </Head>
     <body>
       <Main />
