ca4e33be62767bf997510baa3d9d3ebcc3dfd984
Bas Klinkhamer <bas@position-fixed.nl>
2022-10-02 10:56:29 +0200

    Add a blog overview page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import type { NextPage } from 'next';
 
 import Header from '../../components/header';
 import Footer from '../../components/footer';
+import examplePosts from '../../mocks/blog';
+
 import styles from './blog.module.css';
 
 const BlogOverview: NextPage = () => {
@@ -14,6 +17,17 @@ const BlogOverview: NextPage = () => {
     <Header />
     <main className={styles.overview}>
       <h1 className={styles.overviewTitle}>Blogposts</h1>
+      { examplePosts.map((post, index) => (
+          <Link href={post.link} key={`post--${index}`}>
+            <a className={styles.postItem}>
+              <h2 className={styles.postTitle}>{post.title}</h2>
+              <p className={styles.postIntro}>{post.intro}</p>
+              <div>
+                <button className={styles.postButton}>Read More</button>
+              </div>
+            </a>
+          </Link>
+        )) }
     </main>
     <Footer />
   </>;
