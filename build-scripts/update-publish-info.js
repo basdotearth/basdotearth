57a48f985e7bd09dd10055565db21ac148416d4b
Bas Klinkhamer <bas@position-fixed.nl>
2022-12-02 10:28:21 +0100

    Add staging hook

---
+++ build-scripts/update-publish-info.js
@@ -0,0 +1,23 @@
+const { argv } = require('process');
+const matter = require('gray-matter');
+const { writeFile } = require('fs/promises');
+
+const updatePublishInfo = async () => {
+  const [, , ...mdFilePaths] = argv;
+
+  for await (const path of mdFilePaths) {
+    const file = matter.read(path);
+
+    if (file.data.isPublished === true) {
+      file.data = {
+        ...file.data,
+        updatedOn: new Date().toISOString(),
+      };
+
+      const updatedFileContent = matter.stringify(file);
+      await writeFile(path, updatedFileContent);
+    }
+  }
+};
+
+await updatePublishInfo();
