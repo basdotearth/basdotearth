b017ee01dd50766cdac167dcba772d478c6e4e19
Bas Klinkhamer <bas@position-fixed.nl>
2022-12-02 12:18:32 +0100

    Add favicon, move meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
-import { type AppProps } from 'next/app';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
 import '../styles/reset.css';
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 };
 
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,9 +23,11 @@ const Document = () => {
 
   return <Html>
     <Head>
-      <meta charSet="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=next" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=next" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=next" />
+      <link rel="manifest" href="/site.webmanifest?v=next" />
+      <link rel="shortcut icon" href="/favicon.ico?v=next" />
       <link href={generateGoogleUrl(fonts)} rel="stylesheet" />
     </Head>
     <body>
