d8b69b11831b1cf229aabd0c957fa1bb6e8214ce
Bas Klinkhamer <bas@position-fixed.nl>
2022-12-02 09:16:21 +0100

    Add timestamp to blog

--- src/components/Pages/SinglePost/index.tsx
+++ src/components/Pages/SinglePost/index.tsx
@@ -16,12 +16,18 @@ import styles from './SinglePost.module.css';
 
 const SinglePost: MDXStaticPage<BlogPostMeta | PlaygroundMeta> = ({ code, meta }) => {
   const BlogContent = useMemo(() => getMDXComponent(code), [code]);
+  const timestamp = useMemo(() => {
+    const date = new Date(meta.publishedOn);
+    const tz: [string, { timeZone: string }] = ['nl-NL', { timeZone: 'CET' }];
+    return `${date.toLocaleDateString(...tz)} @ ${date.toLocaleTimeString(...tz)}`;
+  }, [meta.publishedOn]);
 
   return <>
     <Head title={meta.title} description={meta.seoTitle} />
     <Header />
     <main className={styles.main}>
       <h1 className={styles.title}>{ meta.title }</h1>
+      <p className={styles.published}>published { timestamp }</p>
       {'abstract' in meta && <p className="lead">{ meta.abstract }</p>}
       <BlogContent components={{ CodeExample, code: CodeSnippet }}/>
       {'tags' in meta && <TagList tags={meta.tags} />}
