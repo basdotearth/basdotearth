5b0b10efa0d49edf586fa5a3386c4771408e487b
Bas Klinkhamer <bas@position-fixed.nl>
2022-12-22 18:24:16 +0100

    Update typings

--- src/helpers/promises.ts
+++ src/helpers/promises.ts
@@ -1,10 +1,13 @@
-type ResolvePromisesObject = (input: Record<string, Promise<unknown>>) => Promise<Record<string, unknown>>;
+type UnresolvedObject = Record<PropertyKey, Promise<any>>;
+type ResolvedObject<Type extends UnresolvedObject> = { [Key in keyof Type]: Awaited<Type[Key]> };
 
-export const resolvePromisesObject: ResolvePromisesObject = async (input) => {
+export const resolvePromisesObject = async <Type extends UnresolvedObject>(
+  input: Type
+): Promise<ResolvedObject<Type>> => {
   const keys = Object.keys(input);
   const results = await Promise.all(Object.values(input));
   return keys.reduce((output, key, index) => ({
     ...output,
     [key]: results[index],
-  }), {});
+  }), {} as ResolvedObject<Type>);
 };
